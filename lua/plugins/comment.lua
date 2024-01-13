dae5f8ae0554834a1717551c9ecb2d850af86a25
Michael Ridley <litridl@gmail.com>
2024-01-13 17:24:27 +0600

    Migrate nvim-ts-context-commentstring and explicitly list nvim-treesitter-textobjects options

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -6,67 +6,41 @@ local M = {
   "numToStr/Comment.nvim",
   enabled = true,
   event = "BufRead",
-  -- Tsx/jsx support: https://github.com/JoosepAlviste/nvim-ts-context-commentstring
   dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-  opts = {
-    -- Enable keybindings. If given `false` then the plugin won't create any mappings on its own
-    mappings = {
-      -- Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
-      basic = true,
-      -- Extra mapping; `gco`, `gcO`, `gcA`
-      extra = true,
-    },
-    -- Add a space b/w comment and the line
-    padding = true,
-    -- Whether the cursor should stay at its position
-    sticky = true,
-    -- Lines to be ignored while (un)comment
-    ignore = nil,
-    -- LHS of toggle mappings in NORMAL mode
-    toggler = {
-      line = "gcc", -- Line-comment toggle keymap
-      block = "gbc", -- Block-comment toggle keymap
-    },
-    ---LHS of operator-pending mappings in NORMAL and VISUAL mode
-    opleader = {
-      line = "gc", -- Line-comment keymap
-      block = "gb", -- Block-comment keymap
-    },
-    ---LHS of extra mappings
-    extra = {
-      above = "gcO", -- Add comment on the line above
-      below = "gco", -- Add comment on the line below
-      eol = "gcA", -- Add comment at the end of line
-    },
-    pre_hook = nil,
-  },
+  opts = function()
+    return {
+      -- Enable keybindings. If given `false` then the plugin won't create any mappings on its own
+      mappings = {
+        -- Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+        basic = true,
+        -- Extra mapping; `gco`, `gcO`, `gcA`
+        extra = true,
+      },
+      -- Add a space b/w comment and the line
+      padding = true,
+      -- Whether the cursor should stay at its position
+      sticky = true,
+      -- Lines to be ignored while (un)comment
+      ignore = nil,
+      -- LHS of toggle mappings in NORMAL mode
+      toggler = {
+        line = "gcc", -- Line-comment toggle keymap
+        block = "gbc", -- Block-comment toggle keymap
+      },
+      ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+      opleader = {
+        line = "gc", -- Line-comment keymap
+        block = "gb", -- Block-comment keymap
+      },
+      ---LHS of extra mappings
+      extra = {
+        above = "gcO", -- Add comment on the line above
+        below = "gco", -- Add comment on the line below
+        eol = "gcA", -- Add comment at the end of line
+      },
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+    }
+  end,
 }
-
-function M.config(_, opts)
-  opts.pre_hook = function(ctx)
-    -- Only calculate commentstring for tsx filetypes
-    if vim.bo.filetype == "typescriptreact" then
-      local U = require "Comment.utils"
-
-      -- Determine whether to use linewise or blockwise commentstring
-      local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
-
-      -- Determine the location where to calculate commentstring from
-      local location = nil
-      if ctx.ctype == U.ctype.blockwise then
-        location = require("ts_context_commentstring.utils").get_cursor_location()
-      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-        location = require("ts_context_commentstring.utils").get_visual_start_location()
-      end
-
-      return require("ts_context_commentstring.internal").calculate_commentstring {
-        key = type,
-        location = location,
-      }
-    end
-  end
-
-  require("Comment").setup(opts)
-end
 
 return M
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,6 +9,7 @@ local M = {
   event = { "BufReadPost", "BufNewFile" },
   dependencies = {
     "JoosepAlviste/nvim-ts-context-commentstring",
+    "nvim-treesitter/nvim-treesitter-textobjects",
     "nvim-tree/nvim-web-devicons",
   },
   opts = {
@@ -18,9 +19,14 @@ local M = {
     sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
 
     highlight = {
-      enable = true, -- false will disable the whole extension
-      -- disable = { "css" }, -- list of language that will be disabled
+      enable = true,
+      disable = function(_, bufnr)
+        return vim.api.nvim_buf_line_count(bufnr) > 10000
+      end,
     },
+    -- autotag = {
+    --   enable = true,
+    -- },
     autopairs = {
       enable = true,
     },
@@ -42,6 +48,88 @@ local M = {
         scope_incremental = false,
         node_decremental = "<bs>",
       },
+    },
+
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["ak"] = { query = "@block.outer", desc = "around block" },
+          ["ik"] = { query = "@block.inner", desc = "inside block" },
+          ["ac"] = { query = "@class.outer", desc = "around class" },
+          ["ic"] = { query = "@class.inner", desc = "inside class" },
+          ["a?"] = { query = "@conditional.outer", desc = "around conditional" },
+          ["i?"] = { query = "@conditional.inner", desc = "inside conditional" },
+          ["af"] = { query = "@function.outer", desc = "around function " },
+          ["if"] = { query = "@function.inner", desc = "inside function " },
+          ["al"] = { query = "@loop.outer", desc = "around loop" },
+          ["il"] = { query = "@loop.inner", desc = "inside loop" },
+          ["aa"] = { query = "@parameter.outer", desc = "around argument" },
+          ["ia"] = { query = "@parameter.inner", desc = "inside argument" },
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+          ["]k"] = { query = "@block.outer", desc = "Next block start" },
+          ["]f"] = { query = "@function.outer", desc = "Next function start" },
+          ["]a"] = { query = "@parameter.inner", desc = "Next argument start" },
+        },
+        goto_next_end = {
+          ["]K"] = { query = "@block.outer", desc = "Next block end" },
+          ["]F"] = { query = "@function.outer", desc = "Next function end" },
+          ["]A"] = { query = "@parameter.inner", desc = "Next argument end" },
+        },
+        goto_previous_start = {
+          ["[k"] = { query = "@block.outer", desc = "Previous block start" },
+          ["[f"] = { query = "@function.outer", desc = "Previous function start" },
+          ["[a"] = { query = "@parameter.inner", desc = "Previous argument start" },
+        },
+        goto_previous_end = {
+          ["[K"] = { query = "@block.outer", desc = "Previous block end" },
+          ["[F"] = { query = "@function.outer", desc = "Previous function end" },
+          ["[A"] = { query = "@parameter.inner", desc = "Previous argument end" },
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          [">K"] = { query = "@block.outer", desc = "Swap next block" },
+          [">F"] = { query = "@function.outer", desc = "Swap next function" },
+          [">A"] = { query = "@parameter.inner", desc = "Swap next argument" },
+        },
+        swap_previous = {
+          ["<K"] = { query = "@block.outer", desc = "Swap previous block" },
+          ["<F"] = { query = "@function.outer", desc = "Swap previous function" },
+          ["<A"] = { query = "@parameter.inner", desc = "Swap previous argument" },
+        },
+      },
+    },
+
+    playground = {
+      enable = false,
+      disable = {},
+      updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+      persist_queries = false, -- Whether the query persists across vim sessions
+      keybindings = {
+        toggle_query_editor = "o",
+        toggle_hl_groups = "i",
+        toggle_injected_languages = "t",
+        toggle_anonymous_nodes = "a",
+        toggle_language_display = "I",
+        focus_language = "f",
+        unfocus_language = "F",
+        update = "R",
+        goto_node = "<cr>",
+        show_help = "?",
+      },
+    },
+    rainbow = {
+      enable = false,
+      extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+      max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
     },
   },
 }
