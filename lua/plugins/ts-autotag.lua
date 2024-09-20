ca9088be5ca73be97492abb72efbe01375c128ac
Michael Ridley <litridl@gmail.com>
2024-09-19 21:56:47 -0400

    feat(treesitter): add nvim-ts-autotag for HTML tag auto-close/rename

---
+++ lua/plugins/ts-autotag.lua
@@ -0,0 +1,10 @@
+-- Use treesitter to auto close and auto rename html tag
+-- https://github.com/windwp/nvim-ts-autotag
+
+local M = {
+  "windwp/nvim-ts-autotag",
+  enabled = true,
+  opts = {},
+}
+
+return M
